710c014c5baf9ca4315a423d25d0b2a1577bb0ef
ikl0 <klimovs2008van@gmail.com>
2023-11-30 23:15:37 +0300

    add spec for Exchange rate

---
+++ spec/models/exchange_rate_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ExchangeRate, type: :model do
+  it { should validate_presence_of(:rate) }
+  it { should validate_presence_of(:date) }
+  it { should validate_numericality_of(:rate) }
+  it { should belong_to(:currency) }
+end
