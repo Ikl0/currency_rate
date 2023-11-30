8a4953800683fee8d0ebb410a6b9b9f78e72a718
ikl0 <klimovs2008van@gmail.com>
2023-11-30 18:59:51 +0300

    rewrite update method for current day

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -8,26 +8,23 @@ class Currency < ApplicationRecord
   def self.update_exchange_rates
     %w[USD EUR CNY].each do |currency_name|
       currency = find_or_create_by(name: currency_name)
+      date = Date.today
+      url = URI("https://www.cbr-xml-daily.ru/archive/#{date.year}/#{date.strftime('%m')}/#{date.strftime('%d')}/daily_json.js")
 
-      (1..30).each do |i|
-        date = Date.today - i
-        url = URI("https://www.cbr-xml-daily.ru/archive/#{date.year}/#{date.strftime('%m')}/#{date.strftime('%d')}/daily_json.js")
-
-        begin
-          response = Net::HTTP.get(url)
-          data = JSON.parse(response)
-          if data["Valute"] && data["Valute"][currency_name]
-            rate = data["Valute"][currency_name]["Value"]
-          else
-            rate = 0
-          end
-        rescue
+      begin
+        response = Net::HTTP.get(url)
+        data = JSON.parse(response)
+        if data["Valute"] && data["Valute"][currency_name]
+          rate = data["Valute"][currency_name]["Value"]
+        else
           rate = 0
         end
+      rescue
+        rate = 0
+      end
 
-        unless currency.exchange_rates.exists?(date: date)
-          currency.exchange_rates.create(rate: rate, date: date)
-        end
+      unless currency.exchange_rates.exists?(date: date)
+        currency.exchange_rates.create(rate: rate, date: date)
       end
     end
   end
