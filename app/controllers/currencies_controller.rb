7d5ca6f53255f399080216644b78543ea3cc5ac7
ikl0 <klimovs2008van@gmail.com>
2023-11-30 22:13:13 +0300

    refactor weekly action

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -11,8 +11,8 @@ class CurrenciesController < ActionController::Base
   def weekly_rates
     @currencies = Currency.all
     @weeks = 4.times.map do |i|
-      start_date = (i+1).weeks.ago.at_beginning_of_week - 2.days
-      end_date = (i+1).weeks.ago.at_end_of_week - 1.day
+      start_date = (i+1).weeks.ago.prev_occurring(:saturday)
+      end_date = start_date + 1.week
       [start_date, end_date]
     end.reverse
   end
