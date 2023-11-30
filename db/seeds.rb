404d1fdfa1c470200a3d7c939051760800d59ed7
ikl0 <klimovs2008van@gmail.com>
2023-11-30 22:13:50 +0300

    refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,7 @@
-Currency.all.each do |currency|
-  (1..30).each do |i|
-    date = Date.today - i
-    url = URI("https://www.cbr-xml-daily.ru/archive/#{date.year}/#{date.strftime('%m')}/#{date.strftime('%d')}/daily_json.js")
+%w[USD EUR CNY].each do |currency_name|
+  Currency.create(name: currency_name)
+end
 
-    begin
-      response = Net::HTTP.get(url)
-      data = JSON.parse(response)
-      if data["Valute"] && data["Valute"][currency.name]
-        rate = data["Valute"][currency.name]["Value"]
-      else
-        rate = 0
-      end
-    rescue
-      rate = 0
-    end
-
-    unless currency.exchange_rates.exists?(date: date)
-      currency.exchange_rates.create(rate: rate, date: date)
-    end
-  end
+(Date.today - 1.month..Date.today).each do |date|
+  Currency.update_exchange_rates(date)
 end
