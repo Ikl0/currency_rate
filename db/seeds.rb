f488ba01e856717ebd42a5873ebbb3c6e1a46ab6
ikl0 <klimovs2008van@gmail.com>
2023-12-06 01:47:51 +0300

    refactor seeds with threads, add benchmark

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
-%w[USD EUR CNY].each do |currency_name|
-  Currency.create(name: currency_name)
+require 'benchmark'
+require 'concurrent'
+
+time = Benchmark.measure do
+  %w[USD EUR CNY].each do |currency_name|
+    Currency.create(name: currency_name)
+  end
+
+  pool = Concurrent::FixedThreadPool.new(4)
+
+  (Date.today - 1.month..Date.today).each do |date|
+    Concurrent::Future.execute(executor: pool) do
+      Currency.update_exchange_rates(date)
+    end
+  end
+
+  pool.shutdown
+  pool.wait_for_termination
 end
 
-(Date.today - 1.month..Date.today).each do |date|
-  Currency.update_exchange_rates(date)
-end
+puts time.real
